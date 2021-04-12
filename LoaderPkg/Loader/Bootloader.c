3dac0d49917227fed1d82df077f9fd62d28752ff
Vitaly Cheptsov <cheptsov@ispras.ru>
2021-04-12 03:13:30 +0300

    lab7: initial version

--- LoaderPkg/Loader/Bootloader.c
+++ LoaderPkg/Loader/Bootloader.c
@@ -935,7 +935,6 @@ TranslateVirtualAddresses (
   IN VOID       *Context
   )
 {
-#ifdef JOS_SUPPORTS_MEMMAP
   EFI_STATUS     Status;
   LOADER_PARAMS  *LoaderParams;
 
@@ -983,7 +982,6 @@ TranslateVirtualAddresses (
   ASSERT_EFI_ERROR (Status);
   Status = gRT->ConvertPointer (EFI_OPTIONAL_PTR, (VOID **)&LoaderParams->StringTableEnd);
   ASSERT_EFI_ERROR (Status);
-#endif
 }
 
 /**
